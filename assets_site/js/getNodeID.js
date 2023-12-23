429a4e838500d07e006363885b63d15d5973a30e
tijslangeveld <tijslangeveld@gmail.com>
2023-12-23 17:41:38 +0100

    init commit

---
+++ assets_site/js/getNodeID.js
@@ -0,0 +1,35 @@
+function getNodeID (e) {switch (e) {case 'Noord-Brabant':
+      return 'NB';case 'Gelderland':
+      return 'GL';case 'Groningen':
+      return 'GR';case 'Overijssel':
+      return 'OV';case 'Zuid-Holland':
+      return 'ZH';case 'Drenthe':
+      return 'DR';case 'Zeeland':
+      return 'ZE';case 'Flevoland':
+      return 'FL';case 'Limburg':
+      return 'LB';case 'Utrecht':
+      return 'UT';case 'Noord-Holland':
+      return 'NH';case 'Fryslan':
+      return 'FR';case 'Nederland':
+      return 'NL';case 'Wind-Op-Zee':
+      return 'NZ';case 'NB':
+      return 'Noord-Brabant';case 'GL':
+      return 'Gelderland';case 'GR':
+      return 'Groningen';case 'OV':
+      return 'Overijssel';case 'ZH':
+      return 'Zuid-Holland';case 'DR':
+      return 'Drenthe';case 'ZE':
+      return 'Zeeland';case 'FL':
+      return 'Flevoland';case 'LB':
+      return 'Limburg';case 'UT':
+      return 'Utrecht';case 'NH':
+      return 'Noord-Holland';case 'FR':
+      return 'Fryslan';case 'NL':
+      return 'Nederland';case 'NZ':
+      return 'Wind-Op-Zee';case 'GB':
+      return 'GB';case 'NO':
+      return 'NO';case 'DK':
+      return 'DK';case 'DE':
+      return 'DE';case 'BE':
+      return 'BE';default:
+      return 'XX'}}
